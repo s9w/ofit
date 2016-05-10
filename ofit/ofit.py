9fe3491b5848bdb167eb34d38d8592a80c6d0415
Sebastian Werhausen <swerhausen@googlemail.com>
2016-05-10 10:48:04 +0200

    changed to 4-dim model and numpy arrays

--- ofit/ofit.py
+++ ofit/ofit.py
@@ -12,7 +12,7 @@ class Schematic(object):
 
 class Component(object):
     def __init__(self):
-        self.matrix = Matrix([[1, 0], [0, 1]])
+        self.matrix = np.eye(4, dtype=sympy.symbol.Symbol)
         self.schematics = []
 
     def __mul__(self, other):
@@ -27,6 +27,12 @@ class Component(object):
             code += schematic.draw_fun(top_left=curr_pos)
             curr_pos += np.array([schematic.width, 0])
         pass
+
+    def shift_matrix(self, direction):
+        shift_amount = {"up": 1, "down": -1}[direction]
+
+        self.matrix = np.roll(self.matrix, shift=shift_amount, axis=0)
+        self.matrix = np.roll(self.matrix, shift=shift_amount, axis=1)
 
 
 def draw_coupler(top_left):
@@ -44,46 +50,58 @@ def draw_coupler(top_left):
 
 def create_coupler():
     comp_coupler = Component()
-    comp_coupler.matrix = sympy.sqrt(0.5) * Matrix([[1.0, -1j], [-1j, 1.0]])
+    # comp_coupler.matrix = np.eye(4)
+    core_matrix = np.sqrt(0.5) * np.array([[1.0, -1j], [-1j, 1.0]])
+    comp_coupler.matrix[1:3, 1:3] = core_matrix
 
-    schematic_coupler = Schematic()
-    schematic_coupler.draw_fun = draw_coupler
-    schematic_coupler.width = 3
-    comp_coupler.schematics.append(schematic_coupler)
+    # schematic_coupler = Schematic()
+    # schematic_coupler.draw_fun = draw_coupler
+    # schematic_coupler.width = 3
+    # comp_coupler.schematics.append(schematic_coupler)
     return comp_coupler
 
 
 def create_delay(location="top"):
     comp_delay = Component()
+    # comp_delay.matrix = sympy.eye(4)
+
     zm1 = Symbol("zm1")
     if location == "top":
-        comp_delay.matrix = Matrix([[zm1, 0], [0, 1]])
+        core_matrix = np.array([[zm1, 0], [0, 1]])
     elif location == "bottom":
-        comp_delay.matrix = Matrix([[1, 0], [0, zm1]])
+        core_matrix = np.array([[1, 0], [0, zm1]])
     else:
         raise ValueError
+
+    comp_delay.matrix[1:3, 1:3] = core_matrix
+    # comp_delay.matrix = sympy.matrix2numpy(comp_delay.matrix)
+
     return comp_delay
 
 
 def create_phase(delay_param, location="top"):
     comp_phase = Component()
+    # comp_phase.matrix = sympy.eye(4)
+
     phi = Symbol(delay_param)
     if location == "top":
-        comp_phase.matrix = Matrix([[exp(-1j * phi), 0], [0, 1]])
+        core_matrix = np.array([[exp(-1j * phi), 0], [0, 1]])
     elif location == "bottom":
-        comp_phase.matrix = Matrix([[1, 0], [0, exp(-1j * phi)]])
+        core_matrix = np.array([[1, 0], [0, exp(-1j * phi)]])
     else:
         raise ValueError
+
+    comp_phase.matrix[1:3, 1:3] = core_matrix
+    # comp_phase.matrix = sympy.matrix2numpy(comp_phase.matrix)
     return comp_phase
 
 
-coupler = create_coupler()
-# coupler.draw()
-delay = create_delay(location="top")
-phase = create_phase(delay_param="phi_1")
-
-
 def f1():
+    coupler = create_coupler()
+    # coupler.draw()
+    delay = create_delay(location="top")
+    phase = create_phase(delay_param="phi_1")
+
     block = phase * delay * coupler
     pprint(block.matrix)
 

7a3244aae388defe1f4ae382e3a393fc8f13cd2d
s9w <swerhausen@googlemail.com>
2017-04-21 08:32:41 +0200

    updated example and readme to make more sense

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-#ofit: **o**ptical **fi**lter **t**ool
+# ofit: **o**ptical **fi**lter **t**ool
 
-This python program is designed to assist in the design and handling of optical filters. It can create filters that consist of couplers, phase shifters, delay elements, ring elements and crossing elements. From those, more complex structures can be constructed. Common composites like a (symmetric and asymmetric) Mach–Zehnder interferometer are available, too.
+This python program is designed to assist in the design and handling of optical filters. It can create filters that consist of couplers, phase shifters, delay elements, ring elements and crossing elements. From those, more complex structures can be constructed. Common composites like a (symmetric and asymmetric) Mach–Zehnder interferometer are available, too. Since this is a very specific topic in my research field, feel free to contact me or see my thesis for orientation ([s9w.io](http://www.s9w.io/))
 
 The filters can then be drawn (via TikZ) and the transmission matrix containing the transfer functions be calculated.
 
 ## usage
-There is an `example.py` file with some examples. Running it with `python example.py` will write the matrix and the image in the given filenames. The TikZ image can be used like any other TikZ code, an example is in the `example_document.tex`.
+There is an `example.py` file with some examples. Running it with `python example.py` will write the matrix and the image in the given filenames. The TikZ image can be used like any other TikZ code, an example is in the `example_document.tex`. Output of example file is matrix, plus a tex/pdf which should look like this:
+
+![](example.png)
--- example.py
+++ example.py
@@ -1,127 +1,13 @@
-# from ofit import *
 import ofit
-import sympy
-from sympy import Symbol, Matrix, I, simplify
 
-def simple_1():
-    filter_1 = make_coupler()
-    filter_1 = filter_1 * make_delay()
-    filter_1.draw()
+def example(n=2):
+    ofit.options["unit_height"] = 0.5
+    f = ofit.make_mzi() * ofit.make_delay() * ofit.make_phase(shift=-1)
+    for i in range(n - 1):
+        f *= ofit.make_mzi() * ofit.make_delay() * ofit.make_phase(shift=-1)
 
-
-def junguji96(n=3):
-    def generate_unit():
-        return make_phase(draw_sep=True, shift=-1) * make_ring() * make_coupler()
-
-    lattice = make_coupler()
-    for i in range(n):
-        lattice *= generate_unit()
-
-    lattice.draw()
-    ne_matrix = lattice.matrix_to_ne()
-    print(ne_matrix)
+    print(f.matrix)
+    f.draw()
 
 
-    # # print(str(lattice))
-    # h = lattice.matrix[2,2]
-    # print(lattice.matrix.flatten())
-    # # print(type(h))
-    # # print(sympy.mathematica_code(h))
-
-
-def OM04(n=3):
-    def generate_unit():
-        delay = make_delay(draw_sep=True)
-        bottom_phase_inner = make_phase(shift=-1)
-        return delay * bottom_phase_inner * make_mzi()
-
-    lattice = make_phase(shift=-1) * make_mzi()
-    for i in range(n):
-        lattice *= generate_unit()
-
-    lattice.draw()
-
-
-def mimo():
-    lattice = make_coupler(shift=1) * make_coupler(shift=-1)
-
-    phase1 = make_phase(shift=1)
-    phase2 = make_phase()
-    phase3 = make_phase(shift=-1)
-    phase4 = make_phase(shift=-2)
-    lattice *= phase1 * phase2 * phase3 * phase4
-
-    lattice *= make_crosser()
-
-    lattice *= make_coupler(shift=1) * make_coupler(shift=-1)
-
-    lattice.draw()
-    print(str(lattice))
-
-
-def f1():
-    top = make_coupler() * make_phase() * make_coupler()
-    top.shift_up()
-
-    lattice = make_coupler() * make_ring(shift=-1) * top * make_coupler()
-    lattice.draw()
-
-def filter1(n=1):
-    # def make_unit():
-    #     return make_mzi()
-
-    # lattice = make_delay()
-    lattice = make_mzi() * make_delay()* make_phase(shift=-1)
-    for i in range(n-1):
-        lattice *= make_mzi() * make_delay() * make_phase(shift=-1)
-
-    # lattice = make_mzi()
-    ne_matrix = lattice.matrix_to_ne()
-    print(ne_matrix)
-    # lattice.draw()
-
-def test_phis_local():
-    # s1 = OfitState()
-    # filter1 = s1.make_phase()
-    # print(filter1.matrix_to_ne())
-    #
-    # s2 = OfitState()
-    # filter2 = s2.make_phase()
-    # filter2 = s2.make_phase()
-    # print(filter2.matrix_to_ne())
-
-    filter1 = ofit.make_phase()
-    print(filter1.matrix_to_ne())
-    ofit.used_names = set()
-    filter2 = ofit.make_phase()
-    print(filter2.matrix_to_ne())
-    print(ofit.get_symbol_count())
-
-def test_dof():
-    # m1 = ofit.make_coupler() * ofit.make_coupler(shift=1)
-    m1 = ofit.make_ring(shift=-1)
-    print(m1.matrix)
-    print(m1.matrix_to_ne())
-    m1.draw()
-
-    # m1 = c1.matrix[1:3, 1:3]
-    # # sympy.simplify(m1)
-    # print(m1)
-    # # print(c1.matrix_to_ne())
-    # # print(c1.matrix_to_ne())
-    # # print("dof", c1.dof)
-
-    # m1 = simplify(Matrix([[2,0],[0,2]]))
-    # print(m1)
-
-
-# simple_1()
-# junguji96(n=1)
-# OM04(n=3)
-# f1()
-# mimo()
-# filter1(n=4)
-
-# test_phis_local()
-
-test_dof()
+example()
